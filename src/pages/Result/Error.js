a413c58557bfda871b50b0ccc743866c720b93ab
qiuhao <qiuhao@gmail.com>
2018-09-09 09:01:06 +0800

    update

--- src/pages/Result/Error.js
+++ src/pages/Result/Error.js
@@ -1,52 +1,12 @@
 import React, { Fragment } from 'react';
-import { formatMessage, FormattedMessage } from 'umi/locale';
+import router from 'umi/router';
 import { Button, Icon, Card } from 'antd';
 import Result from '@/components/Result';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
-const extra = (
-  <Fragment>
-    <div
-      style={{
-        fontSize: 16,
-        color: 'rgba(0, 0, 0, 0.85)',
-        fontWeight: '500',
-        marginBottom: 16,
-      }}
-    >
-      <FormattedMessage
-        id="app.result.error.hint-title"
-        defaultMessage="The content you submitted has the following error:"
-      />
-    </div>
-    <div style={{ marginBottom: 16 }}>
-      <Icon style={{ color: '#f5222d', marginRight: 8 }} type="close-circle-o" />
-      <FormattedMessage
-        id="app.result.error.hint-text1"
-        defaultMessage="Your account has been frozen"
-      />
-      <a style={{ marginLeft: 16 }}>
-        <FormattedMessage id="app.result.error.hint-btn1" defaultMessage="Thaw immediately" />
-        <Icon type="right" />
-      </a>
-    </div>
-    <div>
-      <Icon style={{ color: '#f5222d', marginRight: 8 }} type="close-circle-o" />
-      <FormattedMessage
-        id="app.result.error.hint-text2"
-        defaultMessage="Your account is not yet eligible to apply"
-      />
-      <a style={{ marginLeft: 16 }}>
-        <FormattedMessage id="app.result.error.hint-btn2" defaultMessage="Upgrade immediately" />
-        <Icon type="right" />
-      </a>
-    </div>
-  </Fragment>
-);
-
 const actions = (
-  <Button type="primary">
-    <FormattedMessage id="app.result.error.btn-text" defaultMessage="Return to modify" />
+  <Button type="primary" onClick={() => router.push('/project')}>
+    返回列表
   </Button>
 );
 
@@ -55,9 +15,7 @@ export default () => (
     <Card bordered={false}>
       <Result
         type="error"
-        title={formatMessage({ id: 'app.result.error.title' })}
-        description={formatMessage({ id: 'app.result.error.description' })}
-        extra={extra}
+        title={'认购失败'}
         actions={actions}
         style={{ marginTop: 48, marginBottom: 16 }}
       />
